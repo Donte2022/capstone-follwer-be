2ec340b9aaa1d466ec501d3df93f4672bb3cc6ec
Dontavious Green <dontavious.o.green@gmail.com>
2022-11-15 07:18:12 -0600

    init

---
+++ src/main/java/yorksolutions/net/capstonefollowerbe/CapstoneFollowerBeApplication.java
@@ -0,0 +1,13 @@
+package yorksolutions.net.capstonefollowerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneFollowerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneFollowerBeApplication.class, args);
+	}
+
+}
